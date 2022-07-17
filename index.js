39b3b8b916f4461dd798bb49dc461a230cd6debc
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-07-17 17:57:57 -0400

    added renderLincenseSection function

--- index.js
+++ index.js
@@ -1,53 +1,58 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const genMark = require('./utils/generateMarkdown');
-const renderBadge = require('./utils/generateMarkdown');
 
 console.log(genMark.method1);
 const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
-    // {
-    //     type: 'input',
-    //     name: 'title',
-    //     message: 'What is the title of your project?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'description',
-    //     message: 'Project description?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'Installation',
-    //     message: 'installation instructions?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'usage',
-    //     message: 'What is this project used for?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'credits',
-    //     message: 'Who worked on this project?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'github',
-    //     message: 'Enter your GitHub link',
-    //   },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Project description?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is this project used for?',
+      },
+      {
+        type: 'input',
+        name: 'credits',
+        message: 'Who worked on this project?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub link',
+      },
       {
         type: 'checkbox',
         name: 'license',
         message: 'What licenses would you like to include?',
-        choices: ['MIT','ISC'],
+        choices: ['MIT','ISC','GPL-3.0','MPL-2.0','Apache-2.0','BSL-1.0', ],
       },
       {
         type: 'input',
         name: 'email',
         message: 'Enter your email',
       },
+      {
+        type: 'input',
+        name: 'fullName',
+        message: 'Enter your full name',
+      },
+      
 
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,18 +6,28 @@ function renderLicenseBadge(license) {
     console.log("RETURN");
     return " ";
   }
-
-  return ` ![badge](` + "https://img.shields.io/badge/license-" + license + "-green" + `)`;
+  console.log("type of " + typeof(license))
+  return ` [![badge](https://img.shields.io/static/v1?label=license&message=`
+  + license 
+  + `&color=green)](https://choosealicense.com/licenses/`
+  + license[0].toLowerCase()
+  +`/)`;
 
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data,license) {
+  var year = new Date().getFullYear();
+  return `
+      ``md
+      Copyright [${year}] [${data.fullName}]
+
+      [copy the [text](https://choosealicense.com/licenses/${license[0].toLowerCase()}) of the license here]
+      ``
+`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown1(data, license) {
@@ -46,18 +56,17 @@ ${data.description}
   ${data.credits}
 
   ## Questions
-  ${data.email}
-  ${data.github}
+  Should you have any questions you can reach me via Github or Email.  
+  ${data.email}  
+ bsl-1.0 ${data.github}
 
-  ## License
-  
-  ${data.license}
-
-  `;
+  ## License `
+  + renderLicenseSection(data, license)
+  + `
+  `
 }
 
 module.exports = {
  method1: generateMarkdown1,
  method2: renderLicenseBadge,
 };
-// module.exports = renderLicenseBadge;
